acb1ade847c52e48e70055ac597062ba5fe4bf49
javaria-2b <145102763+javaria-2b@users.noreply.github.com>
2023-12-20 02:40:00 -0800

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Hero from "@/components/Hero";
+import NewProducts from "@/components/NewProducts";
+import Testimonials from "@/components/Testimonials";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <Hero/>
+      <NewProducts/>
+      <Testimonials/>
+    </main>
+  );
+}
---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-blackish text-gray-500 text-center py-4 pb-16 md:pb-4'>© 2023 Javaria. All rights reserved.</div>
+  )
+}
+
+export default Footer
